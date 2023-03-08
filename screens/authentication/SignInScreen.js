a2e5c057fa5cee66f9a9e194b371f2b21eaed28e
alirazahub <aliraza.ar7785@gmail.com>
2023-03-08 22:57:50 +0500

    Login Screen Styling

--- screens/authentication/SignInScreen.js
+++ screens/authentication/SignInScreen.js
@@ -20,7 +20,32 @@ const SignInScreen = ({ navigation }) => {
         <Text style={styles.logo}>E SHOP</Text>
       </View>
       <View style={{ flex: 0.60 }}>
-        <Text>sa</Text>
+        <View>
+          <Text style={{ marginHorizontal: 10, marginTop: 10, fontSize: 20, color: 'lightgreen', fontWeight: 'bold' }}>Email</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Email"
+            autoCapitalize="none"
+          />
+        </View>
+        <View>
+          <Text style={{ marginHorizontal: 10, marginTop: 10, fontSize: 20, color: 'lightgreen', fontWeight: 'bold' }}>Password</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            autoCapitalize="none"
+            secureTextEntry={true}
+          />
+        </View>
+
+        <View>
+          <TouchableOpacity
+            style={styles.btn}
+            onPress={onSubmit}
+          >
+            <Text style={{textAlign:'center', color:'orange', fontWeight:'bold'}}>Sign In</Text>
+          </TouchableOpacity>
+        </View>
       </View>
       <View style={{ flex: 0.20 }}>
         <Text>sa</Text>
@@ -30,25 +55,36 @@ const SignInScreen = ({ navigation }) => {
 };
 
 const styles = StyleSheet.create({
-  logo:{
-    fontSize:40,
-    fontWeight:'bold',
-    color:'red',
-    alignSelf:'center',
-    borderWidth:1,
-    paddingVertical:5,
-    paddingHorizontal:25,
-    marginTop:7,
-    borderRadius:30
+  logo: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    color: 'lightgreen',
+    alignSelf: 'center',
+    borderWidth: 1,
+    paddingVertical: 5,
+    paddingHorizontal: 25,
+    marginTop: 40,
+    borderRadius: 30,
+    borderColor: 'orange'
   },
   input: {
     padding: 10,
-    width: '90%',
-    marginBottom: 10,
+    margin: 10,
     borderWidth: 1,
-    borderColor: '#ddd',
-    borderRadius: 5
+    borderColor: 'orange',
+    borderRadius: 30,
+    color: 'orange',
+    fontSize: 20
   },
+  btn:{
+    padding: 10,
+    margin: 10,
+    width:100,
+    borderRadius: 30,
+    alignSelf: 'center',
+    borderWidth: 1,
+    borderColor: 'orange',
+  }
 });
 
 export default SignInScreen;
